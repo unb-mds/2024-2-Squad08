3368ffd3ccf2bbba49d2ddb96f6dc70ac36d9447
mach1r0 <segundacontafd@gmail.com>
2024-11-13 17:28:52 -0300

    feat: :fire: Making the navbar and footer

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
+// src/main.tsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import NavBar from './components/navbar.tsx'
+import Footer from './components/footer.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <NavBar />
     <App />
+    <Footer />
   </StrictMode>,
 )
