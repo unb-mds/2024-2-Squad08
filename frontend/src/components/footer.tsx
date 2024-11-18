cf043b1bdd27d7bc148957e50b10c88a016b9d7c
mach1r0 <segundacontafd@gmail.com>
2024-11-18 17:27:36 -0300

    feat: :fire: making the register page and making the navbar responsive

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function footer() {
   return (
-    <footer className="footer footer-center bg-base-300 text-base-content p-4">
+    <footer className="footer footer-center bg-base-300 text-base-content p-4" style={{ backgroundColor: '#1D232A' }}>
       <aside>
-        <p>
+        <p className="text-white">
           Copyright © {new Date().getFullYear()} - All right reserved by MASH1R0 company 
         </p>
       </aside>
