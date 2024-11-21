5eafaa178b9ae64bae931945ecbc1d6f14d37f16
Beatriz Lins <bibi.geane2@gmail.com>
2024-11-21 14:59:58 -0300

    feat: creating logo component in map page

---
+++ frontend/src/components/logo.tsx
@@ -0,0 +1,13 @@
+import "/home/beatriz/Documentos/Github/MOSP-G8/frontend/src/styles/logo.css";
+
+const Logo = () => {
+  return (
+    <div className="logo-background">
+      <button className="logo-button">
+        MONITORA BSB
+      </button>
+    </div>
+  );
+};
+
+export default Logo;
