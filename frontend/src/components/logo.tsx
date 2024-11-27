775c5ba555f274ec9e442d2cd2a9ef4264c5e380
Beatriz Lins <bibi.geane2@gmail.com>
2024-11-27 13:50:51 -0300

    feat: creating logo component

---
+++ frontend/src/components/logo.tsx
@@ -0,0 +1,13 @@
+import "../styles/logo.css";
+
+const Logo = () => {
+  return (
+    <div className="logo-background">
+      <button className="logo-button">
+        MONITORA BSB
+      </button>
+    </div>
+  );
+};
+
+export default Logo;
