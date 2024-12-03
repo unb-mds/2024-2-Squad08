7e8e03d721878da8e10ff8adcd476d83a9b40a68
CerqPaulo <paulocerq675@gmail.com>
2024-12-03 10:53:08 -0300

    Revert "UserProfile"
    This reverts commit 28f9ade803f9ee48c5b6d76a5a83e1c74701e0e8.

--- frontend/src/pages/Auth/UserProfileForm.tsx
+++ frontend/src/pages/Auth/UserProfileForm.tsx
@@ -105,7 +105,7 @@ const UserProfileForm = () => {
                   type="submit"
                   className="bg-blue-500 text-white font-medium px-6 py-2 rounded-lg hover:bg-blue-600 transition"
                 >
-                  Salvar Alteração
+                  Salvar
                 </button>
               </div>
             </form>
