3383547a02819e318f6f07ab6b422a2eb8364fff
Beatriz Lins <bibi.geane2@gmail.com>
2025-01-08 23:04:39 -0300

    feat: error messages for password page and function corrections

--- frontend/src/pages/Senha.tsx
+++ frontend/src/pages/Senha.tsx
@@ -1,10 +1,39 @@
+import React, { useState } from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import "../styles/Registros.css"; 
 import Logo from "../components/Logo";
+import { useNavigate } from "react-router-dom";
 
 export default function Senha() {
-  const position = [-15.7801, -47.9292]; 
+  const position = [-15.7801, -47.9292];
+  const navigate = useNavigate(); 
+
+  const [email, setEmail] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handlePassword = () => {
+    if (!email || !newPassword || !confirmPassword) {
+      setError('Preencha todos os campos!');
+      return;
+    }
+    if (!email.endsWith('@gmail.com')) {
+      setError('Insira um email válido.');
+      return;
+    }
+    if (newPassword.length < 8) {
+      setError('A senha deve ter no mínimo 8 caracteres.');
+      return;
+    }
+    if (newPassword !== confirmPassword) {
+      setError('As senhas não conferem!');
+      return;
+    }
+    setError('');
+    navigate('/endereco'); 
+  };
 
   return (
     <div className="relative h-screen w-full">
@@ -23,11 +52,32 @@ export default function Senha() {
 
       <div className="login">
         <div className="input-container-login password-page">
-            <h1>REDEFINIR SENHA</h1>
-          <input type="text" placeholder='Email' />
-          <input type="text" placeholder='Nova senha' />
-          <input type="text" placeholder='Confirmar senha' />
-          <button className="btn-login">Salvar</button>
+          <h1>REDEFINIR SENHA</h1>
+          <input 
+            type="text" 
+            placeholder="Email" 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)} 
+          />
+          <input 
+            type="password" 
+            placeholder="Nova senha"
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)} 
+          />
+          <input 
+            type="password" 
+            placeholder="Confirmar senha" 
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)} 
+          />
+          {error && <p className="error-message">{error}</p>} {/* Exibe erro, se houver */}
+          <button 
+            className="btn-login"
+            onClick={handlePassword}
+          >
+            Salvar
+          </button>
         </div>
       </div>
     </div>
