4d892c033cfed78da08dbb2c804f59d98be1bee0
Beatriz Lins <bibi.geane2@gmail.com>
2025-01-10 17:59:58 -0300

    feat: creating address logical view

---
+++ backend/app/views/view_endereco.py
@@ -0,0 +1,58 @@
+from flask import Blueprint, jsonify, request
+from app.models.endereco import Endereco
+from app.models import db
+from flask_cors import CORS
+
+endereco_bp = Blueprint('endereco', __name__)
+
+CORS(endereco_bp, resources={
+    r"/*": {
+        "origins": ["http://localhost:5173"],
+        "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        "allow_headers": ["Content-Type", "Authorization"]
+    }
+})
+
+@endereco_bp.route("/endereco", methods=['POST', 'OPTIONS'])
+def cadastrar_endereco():
+    if request.method == 'OPTIONS':
+        return '', 204
+
+    try:
+        data = request.get_json() if request.is_json else request.form
+        cep = data.get('cep')
+        cidade = data.get('cidade')
+        estado = data.get('estado')
+        rua = data.get('rua')
+        bairro = data.get('bairro')
+        numero = data.get('numero')
+
+        if not all([cep, cidade, estado, rua, bairro, numero]):
+            return jsonify({'error': 'Preencha todos os campos'}), 400
+
+        endereco = Endereco(
+            cep=cep,
+            cidade=cidade,
+            estado=estado,
+            rua=rua,
+            bairro=bairro,
+            numero=numero,
+        )
+        db.session.add(endereco)
+        db.session.commit()
+
+        return jsonify({
+            'message': 'Endereço cadastrado com sucesso',
+            'endereco':{
+                'cep': endereco.cep,
+                'cidade': endereco.cidade,
+                'estado': endereco.estado,
+                'rua': endereco.rua,
+                'bairro': endereco.bairro,
+                'numero': endereco.numero,
+            }
+        }), 201
+
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'error': 'Erro inesperado. Tente novamente.'}), 500
