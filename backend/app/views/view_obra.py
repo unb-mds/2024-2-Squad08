2ebed42942cd2ef9a6d1961b34493264679007c1
Mach1r0 <segundacontafd@gmail.com>
2025-01-14 12:00:04 -0300

    feat: :fire:

--- backend/app/views/view_obra.py
+++ backend/app/views/view_obra.py
@@ -1,27 +1,22 @@
-from flask import Blueprint, jsonify, request
-from app.models.obra import Obra
-from app.models import db
-from app.Serializers.obra_serializer import ObraSchema
+# app/views/view_obra.py
+from flask import Blueprint, jsonify
+from ..services.api_consumer import ObraAPIConsumer
+from ..services.obra_service import ObraService
 
 obra_bp = Blueprint('obra', __name__)
 
-obra_schema = ObraSchema()
-obras_schema = ObraSchema(many=True)
-
-@obra_bp.route("/obras", methods=['GET'])
-def get_obras():
-    obras = Obra.query.all()
-    return jsonify(obras_schema.dump(obras))
-
-@obra_bp.route("/obra/<int:id>", methods=['GET'])
-def get_obra(id):
-    obra = Obra.query.get_or_404(id)
-    return jsonify(obra_schema.dump(obra))
-
-@obra_bp.route("/obra", methods=['POST'])
-def create_obra():
-    data = request.get_json()
-    obra = obra_schema.load(data, session=db.session)
-    db.session.add(obra)
-    db.session.commit()
-    return jsonify(obra_schema.dump(obra)), 201
+@obra_bp.route('/sync/<uf>', methods=['POST'])
+def sync_obras(uf):
+    """
+    Endpoint to sync obras for a specific UF
+    """
+    try:
+        consumer = ObraAPIConsumer()
+        service = ObraService(consumer)
+        result = service.sync_obras_for_uf(uf)
+        return jsonify(result)
+        
+    except Exception as e:
+        return jsonify({
+            'error': str(e)
+    }), 500
