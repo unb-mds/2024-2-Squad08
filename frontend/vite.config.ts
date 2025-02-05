5f0f0e0ae205ebe7df2c36c8017c9eac16058c61
Beatriz Lins <bibi.geane2@gmail.com>
2025-02-05 10:19:05 -0300

    fix: including path for tests on vitest config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config"; 
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
@@ -11,9 +11,10 @@ export default defineConfig({
     },
     strictPort: true,
   },
-  test: {
+  test: { 
     globals: true,
     environment: "jsdom",
     setupFiles: "./setupTests.js",
+    include: ["src/tests/**/*.test.{ts,tsx,js,jsx}"], 
   },
 });
