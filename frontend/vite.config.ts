21791078dd98a74510e8a3f787f2d4550819ccfa
Mach1r0 <segundacontafd@gmail.com>
2025-02-12 16:57:11 -0300

    Adding the url on allowedHost

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+ts
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -23,7 +24,12 @@ export default defineConfig(({ mode }) => {
           changeOrigin: true,
           secure: false
         }
-      }
+      },
+      allowedHosts: [
+        'monitorabsb.mash1r0.site',
+        'localhost',
+        '127.0.0.1'
+      ]
     },
     test: {
       globals: true,
