73aee39f1e6288fec6ccf50ecbe087f76304f4a5
Mach1r0 <segundacontafd@gmail.com>
2025-02-12 16:51:35 -0300

    removing the migrations folder and change the init

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -18,20 +18,21 @@ def create_app(config_name="default"):
     
     app = Flask(__name__)
     
+    # Database configuration should be available for all environments
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL',
+        'postgresql://postgres:password@postgres:5432/monitorabsb')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
     if config_name != 'testing':
         CORS(app, resources={r"/*": {"origins": "*"}})
-        app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL',
-            'postgresql://postgres:password@postgres:5432/monitorabsb')
-        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
         app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', '123')
         app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=7)
         jwt = JWTManager(app)
     
     db.init_app(app)
+    migrate = Migrate(app, db)
     
-    if config_name != 'testing':
-        migrate = Migrate(app, db)
-        
+    
     app.register_blueprint(main_bp)
     app.register_blueprint(obra_bp, url_prefix='/obras')
     app.register_blueprint(usuario_bp, url_prefix='/usuario')
